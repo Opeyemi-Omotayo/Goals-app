6a24b6dfb4980fe7b189f4eb4bf2c421388005e6
Opeyemi-Omotayo <opeyemi.motayo1@gmail.com>
2023-10-22 16:59:54 +0200

    styling

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+     <View style={styles.inputContainer}>
+      <TextInput placeholder='Your course goal' style={styles.textInput}/>
+      <Button title='Add Goal' />
+     </View>
+     <View style={styles.gapContainer}>
+      <Text>List of goals</Text>
+     </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20
   },
+  inputContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: 'blue',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    padding: 10,
+    width: '70%',
+    marginRight: 8
+  },
+  gapContainer: {
+    flex: 3,
+  }
 });
